3b0e827aad12b5c52df03b5906a4876f15844e7f
Mutahe <elizagatura@gmail.com>
2024-04-20 14:41:15 -0700

    puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,13 @@
-# Increase the amount of traffic an Nginx server can handle.
+# Increases the amount of traffic an Nginx server can handle.
 
-# Increae the ULIMIT
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  # modify value of ULIMIT
-  command => '/bib/sed -i "s/15/4096/" /etc/default/nginx',
-  # path specification
-  path => 'usr/local/bin/:/bin/',
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => '/etc/init.d nginx restart',
-  path => '/etc/init.d/',
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
 }
